bdbc23c7f7b23fd0f1fe8c4d78d0e28494fd23be
rajeevranjan45526 <91402006+rajeevranjan45526@users.noreply.github.com>
2021-09-26 09:34:44 +0530

    Update app.js

--- BOT-SocksBot/app.js
+++ BOT-SocksBot/app.js
@@ -4,4 +4,4 @@ console.log("Iniciando bot :)");
 
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311284', 1, true, join(__dirname, 'socks.txt'));
+botPoll('https://strawpoll.com/juc6gbs7w', 1, true, join(__dirname, 'socks.txt'));
